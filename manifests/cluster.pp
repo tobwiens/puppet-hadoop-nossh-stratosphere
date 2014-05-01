3ae6caaf48c6880936ceadb0f667bdee315514db
TobiasWiens <tobwiens@gmail.com>
2014-05-01 10:31:01 +0200

    Ban all SSH commands and let puppet start those
    Commands should be idempotent to ensure cluster health over time.

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -9,6 +9,7 @@ class hadoop::cluster::master {
     require hadoop::params
     require hadoop
 
+	
     exec { "Format namenode":
         command => "./hdfs namenode -format",
         user => "${hadoop::params::hadoop_user}",
@@ -16,7 +17,7 @@ class hadoop::cluster::master {
         creates => "${hadoop::params::hadoop_tmp_path}/dfs/name/current/VERSION",
         alias => "format-hdfs",
         path    => ["/bin", "/usr/bin", "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/bin"],
-        before => [Exec["start-namenode"], Exec["start-datanodes"], Exec["start-resourcemanager"], Exec["start-nodemanager"], Exec["start-historyserver"]],
+        before => [Exec["start-namenode"], Exec["start-resourcemanager"],  Exec["start-historyserver"]],
         require => File["hadoop-master"],
     }
 
@@ -26,33 +27,37 @@ class hadoop::cluster::master {
         user => "${hadoop::params::hadoop_user}",
         alias => "start-namenode",
         path    => ["/bin", "/usr/bin", "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin"],
-        before => [Exec["start-datanodes"], Exec["start-resourcemanager"], Exec["start-nodemanager"], Exec["start-historyserver"]],
+        unless => "${java::params::java_base}/jdk${java::params::java_version}/bin/jps | grep  NameNode 2>/dev/null",
+        before => [Exec["start-resourcemanager"],  Exec["start-historyserver"]],
     }
 
-    exec { "Start datanodes":
-        command => "./hadoop-daemons.sh start datanode",
-        cwd => "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin",
-        user => "${hadoop::params::hadoop_user}",
-        alias => "start-datanodes",
-        path    => ["/bin", "/usr/bin", "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin"],
-        before => [Exec["start-resourcemanager"], Exec["start-nodemanager"], Exec["start-historyserver"]],
-    }
+    ### Don't start it over SSH ####
+    #exec { "Start datanodes":
+    #    command => "./hadoop-daemons.sh start datanode",
+    #    cwd => #"${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin",
+    #   user => "${hadoop::params::hadoop_user}",
+    #   alias => "start-datanodes",
+    #   path    => ["/bin", "/usr/bin", #"${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin"],
+    #    before => [Exec["start-resourcemanager"], #Exec["start-historyserver"]],
+    # }
     exec { "Start resourcemanager":
         command => "./yarn-daemon.sh start resourcemanager",
         cwd => "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin",
         user => "${hadoop::params::hadoop_user}",
         alias => "start-resourcemanager",
         path    => ["/bin", "/usr/bin", "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin"],
-        before => [Exec["start-nodemanager"], Exec["start-historyserver"]],
+        unless => "${java::params::java_base}/jdk${java::params::java_version}/bin/jps | grep ResourceManager 2>/dev/null",
+        before => [ Exec["start-historyserver"]],
     }
-    exec { "Start nodemanager":
-        command => "./yarn-daemons.sh start nodemanager",
-        cwd => "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin",
-        user => "${hadoop::params::hadoop_user}",
-        alias => "start-nodemanager",
-        path    => ["/bin", "/usr/bin", "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin"],
-        before => Exec["start-historyserver"],
-    }
+### Don't do it via SSH ###
+    #exec { "Start nodemanagers":
+    #    command => "./yarn-daemons.sh start nodemanager",
+    #    cwd => #"${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin",
+    #    user => "${hadoop::params::hadoop_user}",
+    #    alias => "start-nodemanager",
+    #    path    => ["/bin", "/usr/bin", #"${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin"],
+    #    before => Exec["start-historyserver"],
+    #}
 
     exec { "Start historyserver":
         command => "./mr-jobhistory-daemon.sh start historyserver",
@@ -60,6 +65,7 @@ class hadoop::cluster::master {
         user => "${hadoop::params::hadoop_user}",
         alias => "start-historyserver",
         path    => ["/bin", "/usr/bin", "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin"],
+		unless => "${java::params::java_base}/jdk${java::params::java_version}/bin/jps | grep JobHistoryServer 2>/dev/null",
     }
  
 }
@@ -78,16 +84,16 @@ exec { "Start slave nodemanager":
 unless => "/opt/java/jdk1.7.0_51/bin/jps | grep NodeManager 2>/dev/null",
     }
 
-exec { "Start slave datanode":
+	exec { "Start slave datanode":
         command => "./hadoop-daemon.sh start datanode",
         cwd => "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin",
         user => "${hadoop::params::hadoop_user}",
         alias => "start-slave-datanode",
         path    => ["/bin", "/usr/bin", "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/sbin"],
-unless => "/opt/java/jdk1.7.0_51/bin/jps | grep DataNode 2>/dev/null",
-refresh => "./hadoop-daemon.sh stop datanode;./hadoop-daemon.sh start datanode",
-subscribe => File["${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/conf/core-site.xml"],
-logoutput => true,
+		unless => "${java::params::java_base}/jdk${java::params::java_version}/bin/jps | grep DataNode 2>/dev/null",
+		refresh => "./hadoop-daemon.sh stop datanode;./hadoop-daemon.sh start datanode",
+		subscribe => File["${hadoop::params::hadoop_base}/hadoop-${hadoop::params::version}/conf/core-site.xml"],
+		logoutput => true,
     }
 
 }
